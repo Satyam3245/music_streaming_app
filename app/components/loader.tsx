52e8e35394ce25b4b7b4fdf5f12a3b207f4dd0d9
Satyam <mitravlakshay@gmail.com>
2024-09-21 11:46:10 +0530

    Add Loader file

---
+++ app/components/loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Loader: React.FC = () => {
+  return (
+    <div className="w-10 h-10 border-8 border-t-8 border-white border-opacity-30 rounded-full animate-spin"></div>
+  );
+};
+
